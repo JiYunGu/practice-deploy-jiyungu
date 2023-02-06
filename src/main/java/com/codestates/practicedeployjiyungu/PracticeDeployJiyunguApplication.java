158dab7f39953539f8fc9b206a2ab0bf8c94b2ba
JiYunGu <paker186@gmail.com>
2023-02-06 14:11:35 +0900

    first

---
+++ src/main/java/com/codestates/practicedeployjiyungu/PracticeDeployJiyunguApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicedeployjiyungu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDeployJiyunguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDeployJiyunguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicedeployjiyungu/PracticeDeployJiyunguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicedeployjiyungu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDeployJiyunguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
